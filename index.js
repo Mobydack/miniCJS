0aaf96dec9630192eef163cc70ea59b6d1b452db
Kirill <32056691+Mobydack@users.noreply.github.com>
2019-01-17 22:32:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const compile = value => {
   let css = '';
   for (let rule in value) {
     if ( Object.prototype.toString.call(value[rule]) === '[object Object]' ) {
-      css += `${rule} { ${ObjectToCss(value[rule])} }`;
+      css += `${rule} { ${compile(value[rule])} }`;
     } else {
       css += `${rule}: ${value[rule]};`
     }
   }
   return css;
-}
