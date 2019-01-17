25074d5b91c6b4974bb25d060e28d9bd3c8ce0e5
Kirill <32056691+Mobydack@users.noreply.github.com>
2019-01-18 00:16:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const miniCJS = function compile(CJS) {
-  if ( Object.prototype.toString.call(CJS[rule] === '[object Object]' ) {
+  if ( Object.prototype.toString.call(CJS) !== '[object Object]' ) {
     console.log('Invalid argument');
     return '';
   }
